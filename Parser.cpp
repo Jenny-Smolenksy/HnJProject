9f0315d5bb2ed4c03a7994d8232dd1ba2c0e3625
hilla halevi <hilla464@gmail.com>
2018-12-23 02:20:59 +0200

    boolian expressions , if while set up,lexer parser comments

--- Parser.cpp
+++ Parser.cpp
@@ -4,13 +4,14 @@
 
 #include "Parser.h"
 #include "Expressions/Command/DefineVarCommand.h"
-#include "Expressions/Command/OpenServerComman.h"
+
 #include "Expressions/Command/ConnectCommand.h"
 #include "Expressions/Command/PrintCommand.h"
 #include "Expressions/Command/SleepCommand.h"
 #include "SymbolTable.h"
 #include "Expressions/Command/ConditionParser.h"
 #include "Expressions/Command/LoopComand.h"
+#include "Expressions/Command/OpenServerCommand.h"
 
 Parser *Parser::instance = nullptr;
 
@@ -62,7 +63,7 @@ void Parser::runCommand(deque<string> command) {
     SymbolsTable *s = SymbolsTable::getInstance();
     c = stringToCommandMap[command[0]];
     if (c == nullptr) {
-        if (s->exict(command[0])) {
+        if (s->exist(command[0])) {
             //update existing var
             c = stringToCommandMap["var"];
             c->calculate(command);
